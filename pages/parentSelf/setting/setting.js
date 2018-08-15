e43dd51bfc0a6e8ef2aee03ad58ac4033574fdb1
easytnt-wx <1085518932@qq.com>
2018-08-15 22:43:59 +0800

    yswx

---
+++ pages/parentSelf/setting/setting.js
@@ -0,0 +1,72 @@
+// pages/teacherRegister/teacherRegister.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    regionArray: ["深圳", "北京", "上海", "武汉"],
+    regionIndex: 0
+  },
+  selectregion: function (e) {
+    this.setData({
+      regionIndex: e.detail.value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
